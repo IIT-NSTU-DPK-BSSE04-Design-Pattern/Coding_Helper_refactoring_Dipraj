69ab8d654a5c5bedbf11270f6b6bfd5f42c9db53
dipgit.com <diprajdhar08@gmail.com>
2024-12-01 10:38:43 +0600

    code smell: magic number refactoring: change 600 to 800

--- code_clone/BoxAndWhiskerChart.java
+++ code_clone/BoxAndWhiskerChart.java
@@ -4,10 +4,6 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.Font;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import org.jfree.chart.ChartPanel;
@@ -19,62 +15,54 @@ import org.jfree.chart.renderer.category.BoxAndWhiskerRenderer;
 import org.jfree.data.statistics.DefaultBoxAndWhiskerCategoryDataset;
 
 public class BoxAndWhiskerChart {
-    ChartPanel chartPanel;
+     private static final int CHART_WIDTH = 800;
+     private static final int CHART_HEIGHT = 600;
 
-    JScrollPane scrollPane;
+    // Refactored method to create Box-and-Whisker chart
+    public void displayChart(BoxAndWhiskerChar boxData) {
+        JFrame frame = new JFrame("Clone_Check");
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
-    private List<Double> getInputData(double l[]) {
-        ArrayList<Double> list = new ArrayList<>();
-        for (int j = 0; j < l.length; j++) {
-            double d = l[j];
-            //    System.out.println("k=="+l[j]);
-            list.add(l[j]);
-
-        }//System.out.println("");
-        return list;
-    }
-
-    public void display() {
-        JFrame f = new JFrame("Clone_Check");
-
-        f.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        DefaultBoxAndWhiskerCategoryDataset boxData = new DefaultBoxAndWhiskerCategoryDataset();
-
-        for (int i = 0; i < CosineSimilarity.similarArray.size(); i++) {
-            // System.out.println("p="+CosineSimilarity.similarArray.g);
-            boxData.add(getInputData(CosineSimilarity.similarArray.get(i)), "First_Project vs Second_Project", CloneCheck.ProjectFileName1.get(i));
-        }
+        // Create renderer
         BoxAndWhiskerRenderer renderer = new BoxAndWhiskerRenderer();
         renderer.setFillBox(true);
         renderer.setUseOutlinePaintForWhiskers(true);
-
         renderer.setMedianVisible(true);
         renderer.setMeanVisible(false);
 
+        // Create axes
         CategoryAxis xAxis = new CategoryAxis("First_Project_Files");
         NumberAxis yAxis = new NumberAxis("Second_Project_Values");
+        
+        // Create plot
         CategoryPlot plot = new CategoryPlot(boxData, xAxis, yAxis, renderer);
-        final JFreeChart chart = new JFreeChart(
-                "Box-and-Whisker Plot",
-                new Font("SansSerif", Font.BOLD, 20),
-                plot,
-                true
-        );
-        final ChartPanel chartPanel = new ChartPanel(chart);
+        JFreeChart chart = new JFreeChart("Box-and-Whisker Plot", new Font("SansSerif", Font.BOLD, 20), plot, true);
+
+        // Create chart panel
+        ChartPanel chartPanel = new ChartPanel(chart);
         chart.setBackgroundPaint(Color.LIGHT_GRAY);
-        //   JFreeChart chart = new JFreeChart("Test", JFreeChart.DEFAULT_TITLE_FONT, plot, true);
-        f.add(new ChartPanel(chart) {
-            @Override
-            public Dimension getPreferredSize() {
-                return new Dimension(600, 600);
-            }
-        });
-        f.pack();
-        f.setLocationRelativeTo(null);
-        f.setVisible(true);
+
+        // Configure frame
+        frame.add(chartPanel);
+        frame.setPreferredSize(new Dimension(CHART_WIDTH, CHART_HEIGHT));
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
     }
 
-    public static void BoxWhisker() {
-        EventQueue.invokeLater(new BoxAndWhiskerChart()::display);
+    // Refactored method to create dataset from input data
+    private DefaultBoxAndWhiskerCategoryDataset createDataset(List<List<Double>> data, List<String> fileNames) {
+        DefaultBoxAndWhiskerCategoryDataset dataset = new DefaultBoxAndWhiskerCategoryDataset();
+        for (int i = 0; i < data.size(); i++) {
+            dataset.add(data.get(i), "First_Project vs Second_Project", fileNames.get(i));
+        }
+        return dataset;
+    }
+
+    // Refactored method to be called from main class
+    public static void createAndDisplayChart(List<List<Double>> similarData, List<String> fileNames) {
+        BoxAndWhiskerChart chart = new BoxAndWhiskerChart();
+        DefaultBoxAndWhiskerCategoryDataset dataset = chart.createDataset(similarData, fileNames);
+        EventQueue.invokeLater(() -> chart.displayChart(dataset));
     }
 }
